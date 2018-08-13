a19a5c81b440b6950d41eb4bb08f039662f804a0
JohnDaise <jdaise@gmail.com>
2018-08-13 15:41:22 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
+class Driver {
+  constructor (name, startDate){
+    this.name = name;
+    this.startDate = new Date(startDate);
 
+  };
+
+  yearsExperienceFromBeginningOf(year){
+    return year - this.startDate.getFullYear()-1;
+  };
+
+}
+
+beginningLocation = {horizontal: '1st avenue', vertical: '13th Street'}
+
+class Route {
+  constructor (beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+
+  };
+
+//
+  blocksTravelled(){
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+   let vert = Math.abs(this.endingLocation.vertical - this.beginningLocation.vertical);
+   let horiz  = Math.abs(eastWest.indexOf(this.endingLocation.horizontal) - eastWest.indexOf(this.beginningLocation.horizontal));
+
+
+   return vert + horiz;
+  };
+
+
+estimatedTime(peakHours){
+  if (peakHours){
+      return this.blocksTravelled()/2
+  } else {
+    return this.blocksTravelled()/3
+    };
+  };
+
+
+//
+}
